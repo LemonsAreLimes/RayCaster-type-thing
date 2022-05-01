89a857190be98deadd5e6cbe70026a34c743950f
LemonsAreLimes <96306499+LemonsAreLimes@users.noreply.github.com>
2022-05-01 12:58:53 -0600

    slight tweaks to rendering algoritim
    camera data is interpolated from highest point to lowest (non 0) point, giving a smoother, more angular output

--- Ray_Caster_v2.py
+++ Ray_Caster_v2.py
@@ -4,6 +4,8 @@ import numpy as np
 import math
 import time         #going to be used for calculating render times
 
+
+
 def CreatePointArray(line, obj_res):    #this will return the 'non-real' cords of a line
                                         #this only works on a non-angled line, dont know if im going to change this
     print('creating point array')
@@ -58,7 +60,7 @@ def GetAngle(opp, adj):
     angle = math.atan2(opp, adj)
     return math.degrees(angle)
 
-def RayToDistanceList(RayList, Viewable, res):
+def CamViewable(RayList, Viewable, res):
     DistanceList = []
     for ang in range(Viewable[0]*res, Viewable[1]*res):
         angle = ang/res
@@ -74,7 +76,102 @@ def RayToDistanceList(RayList, Viewable, res):
             DistanceList.append(0)
 
     return DistanceList
-            
+
+def FindHighAndLow(listy):
+    high = [0, 0]   #[value, position]
+    low = [1000, 0]
+
+    zeros = 0
+    for i in range(len(listy)):
+        if listy[i] != 0:
+
+            if listy[i] >  high[0]:
+                high = [listy[i], i]
+
+            if listy[i] < low[0]:
+                low = [listy[i],i]
+
+        else:
+            zeros+=1
+
+    if zeros == len(listy):     #prevents spitting out broken data
+        return False
+    else:
+        return [high, low]
+
+def AvgPoints(inital_list, HighAndLow):
+    High = HighAndLow[0]
+    Low = HighAndLow[1]
+
+            #find the start, end and diffrence 
+    if High[1] > Low[1]:
+        first = HighAndLow[1][0]
+        end = HighAndLow[0][0]
+
+        diff = HighAndLow[0][1] - HighAndLow[1][1]
+        startIndex = HighAndLow[0][1]
+        endIndex = HighAndLow[1][1]
+
+    elif Low[1] > High[1]:
+        first = HighAndLow[0][0]
+        end = HighAndLow[1][0]
+
+        diff = HighAndLow[1][1] - HighAndLow[0][1]
+        startIndex = HighAndLow[0][1]
+        endIndex = HighAndLow[1][1]
+
+    elif Low[1] == High[1]:
+        first = HighAndLow[0][0]
+        end = HighAndLow[1][0]
+
+        diff = HighAndLow[1][1] - HighAndLow[0][1]
+        startIndex = HighAndLow[0][1]
+        endIndex = HighAndLow[1][1]
+
+    else:
+        print("SOMETHING WENT WRONG LINE 404")
+
+        #actually generates the desired data
+    new_range = interpolate(first, end, diff)
+    ini_len = len(inital_list)
+
+        #creates new list the same length as the inital, with new data inserted
+
+    new = []
+    for i in range(ini_len):
+        if i < startIndex:                          #if before new range append inintal list
+            new.append(inital_list[i])
+
+        elif i > endIndex:                          #if after new range append inital list
+            new.append(inital_list[i])
+
+        elif i >= startIndex and i <= endIndex:     #if in list append new
+            z = i - startIndex
+            try:
+                new.append(new_range[z])
+            except IndexError:
+                print('index error')
+
+        else:
+            print('uhhh', i)
+    
+    return new
+
+def interpolate(start, end, diff):
+    x = np.linspace(start, end, diff)
+    y = x.tolist()
+    return y
+
+def smooth_cam_output(Distance_list):
+    HighAndLow = FindHighAndLow(Distance_list)
+    
+    if HighAndLow != False:
+        print(HighAndLow)
+        avg = AvgPoints(Distance_list, HighAndLow)    
+        return avg
+    else:
+        return 0
+
 def Render(map):
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
@@ -83,7 +180,7 @@ def Render(map):
     plt.show()
 
 def RenderAnim(obj, cam_cords, res):
-    fig = plt.figure(figsize=(6,6))
+    fig = plt.figure(figsize=(6, 3))
 
     RayList = GetDist(obj, cam_cords)
 
@@ -93,20 +190,23 @@ def RenderAnim(obj, cam_cords, res):
 
         rot = [FOV[0]+i, FOV[1]+i]
 
-        DistanceList = RayToDistanceList(RayList, rot, res)
+        DistanceList = CamViewable(RayList, rot, res)
+        output = smooth_cam_output(DistanceList)
 
         plt.title(f"Frame: {i}, Angle: 90+{i}")
-        plt.plot(DistanceList)
+        plt.plot(output)
     
     animation = FuncAnimation(fig, animation_func, interval = 1)
     plt.show()
 
 
-#configurations
-FOV = [0,90]
+
+
+#config
 RES = 100
 obj_res = 1
 
+FOV = [1,90]
 cam_cords = [32,32]
 
 #boundary
@@ -116,10 +216,18 @@ TopRight = [64, 64]
 BottomLeft = [0, 0]
 BottomRight = [64, 0]
 
-Top = CreatePointArray([TopLeft, TopRight], obj_res)
-Bottom = CreatePointArray([BottomLeft, BottomRight], obj_res)
 
-bounds = Top + Bottom 
+# obj_list = [[[0, 64],[64, 64]], [[0, 0],[64, 0]]]
+
+
+
+Top = CreatePointArray([TopLeft, TopRight], obj_res)
+print([TopLeft, TopRight])
+
+# Bottom = CreatePointArray([BottomLeft, BottomRight], obj_res)
+# print([BottomLeft, BottomRight])
+
+bounds = Top
 
 RenderAnim(bounds, cam_cords, RES)
 
